c32f2e37490dc43150fd2cfe2efe57d227fbc525
Jessica Arrais <jessicalarrais@gmail.com>
2023-02-22 23:35:31 -0500

    new fake page for the full recipe added

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", (_event) => {
-  const form = document.querySelector(".form");
-  const recipesList = document.querySelector(".container");
+  // Helper array to store data temporarilly
+  let temp = [];
 
+  const form = document.querySelector(".form");
+  const recipesList = document.querySelector(".recipe-list");
+
+  // Form to retrieve data from API
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -15,7 +19,11 @@ window.addEventListener("load", (_event) => {
         },
       })
       .then((res) => {
+        if (res.data.length === 0) {
+          prompt("no results found");
+        }
         recipesList.innerHTML = "";
+        temp = res.data;
         res.data.forEach(createDrinkCard);
       })
       .catch((err) => console.log(err));
@@ -23,7 +31,7 @@ window.addEventListener("load", (_event) => {
     form.reset();
   });
 
-  // Creates a function to validate the fields of the form
+  // Validates the field of the form
   const formInputAuth = (targetName) => {
     const errorSpan = document.querySelector(".form__validity");
 
@@ -37,6 +45,7 @@ window.addEventListener("load", (_event) => {
     return true;
   };
 
+  // Creates a HTML tag
   const createTagNode = (parent, tagType, classes, value = null) => {
     const tagNode = document.createElement(tagType);
     tagNode.className = classes;
@@ -46,25 +55,47 @@ window.addEventListener("load", (_event) => {
     return tagNode;
   };
 
-  const createDrinkCard = (recipe) => {
-    const recipeItem = createTagNode(recipesList, "li", "recipe-card");
+  // Creates a fake page to display the recipe
+  const createRecipePage = (recipe) => {
+    const recipeSection = createTagNode(recipesList, "div", "recipe-section");
+    const recipeItem = createTagNode(recipesList, "li", "recipe-page");
 
-    createTagNode(recipeItem, "h3", "recipe-card__title", recipe.title);
-
-    createTagNode(recipeItem, "div", "recipe-card__ingredients-box");
+    createTagNode(recipeSection, "h3", "recipe", recipe.title);
+    createTagNode(recipeItem, "div", "recipe-page__ingredients-box");
 
     // Splits the ingredients string into an array
     const ingredients = recipe.ingredients.split("|");
     ingredients.forEach((ing) => {
-      createTagNode(recipeItem, "p", "recipe-card__ingredients", ing);
+      createTagNode(recipeItem, "p", "recipe-page__ingredients", ing);
     });
 
     createTagNode(
       recipeItem,
       "p",
-      "recipe-card__instructions",
+      "recipe-page__instructions",
       recipe.instructions.replaceAll(".", ".\n")
     );
+
+    createTagNode(recipeItem, "p", "recipe-page__servings", recipe.servings);
+
+    createTagNode(recipeItem, "button", "recipe-page", "BACK").addEventListener(
+      "click",
+      (_e) => {
+        recipesList.innerHTML = "";
+        temp.forEach(createDrinkCard);
+      }
+    );
+  };
+
+  // Creates a Drink Card
+  const createDrinkCard = (recipe) => {
+    const recipeItem = createTagNode(recipesList, "li", "recipe-card");
+    recipeItem.addEventListener("click", (_e) => {
+      recipesList.innerHTML = "";
+      createRecipePage(recipe);
+    });
+
+    createTagNode(recipeItem, "h3", "recipe-card__title", recipe.title);
 
     createTagNode(recipeItem, "p", "recipe-card__servings", recipe.servings);
   };
